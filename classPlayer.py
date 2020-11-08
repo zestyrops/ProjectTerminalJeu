2ba7d8ea407f85e20c702854d5188222f9045e5a
zestyrops <74087280+zestyrops@users.noreply.github.com>
2020-11-08 15:40:18 +0100

    Update classPlayer.py
    Ajout des collisions

--- classPlayer.py
+++ classPlayer.py
@@ -16,42 +16,53 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 540
         self.rect.y = 300
+        
+    def rect_obstacles(self):
+        return [{'x':self.game.all_objects.sprites()[i].rect.x, 'y':self.game.all_objects.sprites()[i].rect.y, 'width':self.game.all_objects.sprites()[i].rect.width, 'height':self.game.all_objects.sprites()[i].rect.height} for i in range(len(self.game.all_objects))]
 
     # Méthode qui permet de faire bouger le joueur a droite :
     def moveRigth(self):
         # Si le joueur est en collision avec le cercueille :
-        if not self.game.check_collision(self, self.game.all_objects):
+        deplacement = True
+        for obstacle in self.rect_obstacles():
+            if not (self.rect.y >= obstacle['y']+obstacle['height'] or self.rect.y+self.rect.height <= obstacle['y'] or self.rect.x+self.rect.width <= obstacle['x'] or self.rect.x >= obstacle['x']+obstacle['width']-self.velocity):
+                deplacement = False
+        if deplacement:
             self.rect.x += self.velocity
             self.marche.play()
-        else:
-            self.rect.x -= self.velocity
 
     # Méthode qui permet de faire bouger le joueur a gauche :
     def moveLef(self):
         # Si le joueur est en collision avec le cercueille :
-        if not self.game.check_collision(self, self.game.all_objects):
+        deplacement = True
+        for obstacle in self.rect_obstacles():
+            if not (self.rect.y >= obstacle['y']+obstacle['height'] or self.rect.y+self.rect.height <= obstacle['y'] or self.rect.x+self.rect.width <= obstacle['x']+self.velocity or self.rect.x >= obstacle['x']+obstacle['width']):
+                deplacement = False
+        if deplacement:
             self.rect.x -= self.velocity
             self.marche.play()
-        '''else:
-            self.rect.x += self.velocity'''
 
     # Méthode qui permet de faire bouger le joueur en haut :
     def moveUp(self):
         # Si le joueur est en collision avec le cercueille :
-        if not self.game.check_collision(self, self.game.all_objects):
+        deplacement = True
+        for obstacle in self.rect_obstacles():
+            if not (self.rect.y >= obstacle['y'] + obstacle['height'] + self.velocity or self.rect.y + self.rect.height <= obstacle['y'] or self.rect.x + self.rect.width <= obstacle['x'] or self.rect.x >= obstacle['x'] + obstacle['width']):
+                deplacement = False
+        if deplacement:
             self.rect.y -= self.velocity
             self.marche.play()
-        else:
-            self.rect.y += self.velocity
 
     # Méthode qui permet de faire bouger le joueur en bas:
     def moveDown(self):
         # Si le joueur est en collision avec le cercueille :
-        if not self.game.check_collision(self, self.game.all_objects):
+        deplacement = True
+        for obstacle in self.rect_obstacles():
+            if not (self.rect.y >= obstacle['y'] + obstacle['height'] or self.rect.y + self.rect.height <= obstacle['y'] - self.velocity or self.rect.x + self.rect.width <= obstacle['x'] or self.rect.x >= obstacle['x'] + obstacle['width']):
+                deplacement = False
+        if deplacement:
             self.rect.y += self.velocity
             self.marche.play()
-        else:
-            self.rect.y -= self.velocity
 
         # Création d'une méthode qui permet d'afficher la barre de vie du joueur :
     def update_health_bar(self, surface):
